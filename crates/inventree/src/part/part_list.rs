871e9c1579522e68a00a06409cde30bd3084e982
Tim <tigga01@online.de>
2025-08-29 04:24:09 +0200

    Refactor PartListQuery and update API request handling in PartRepository

--- crates/inventree/src/part/part_list.rs
+++ crates/inventree/src/part/part_list.rs
@@ -1,19 +1,19 @@
-use serde::Serialize;
 use crate::part::{InventreePart, PartRepository};
+use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct PartListQuery {
     pub category: Option<u64>,
-    pub parameters: Option<bool>
+    pub parameters: Option<bool>,
 }
 
 impl PartRepository {
     pub async fn list(&self, query: &PartListQuery) -> anyhow::Result<Vec<InventreePart>> {
         let url = format!("{}/api/part/", self.0.base_url);
-        let client = reqwest::Client::new();
-        let response = client
+        let response = self
+            .0
+            .client
             .get(&url)
-            .header("Authorization", format!("Token {}", self.0.api_key))
             .query(query)
             .send()
             .await?
